5817d98f0fb0bf3deda4c5f29b247e75f532b875
sofiulaloe <sahasofiulalom3@gmail.com>
2024-06-27 18:08:16 +0530

    seatcontainer add file  li create alement add

--- js/add.js
+++ js/add.js
@@ -4,7 +4,19 @@ let count = 0;
 for (const btn of allbtn) {
     btn.addEventListener('click', function (e) {
         count = count + 1;
-      
+        const seatName = e.target.childNodes[0].data;
+        const seatPrice = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[3].childNodes[5].childNodes[3].childNodes[0].innerText;
+        
+        const seatContainer = document.getElementById('Seat-container');
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        p.innerText = seatName;
+        const p2 = document.createElement('p');
+        p2.innerText = seatPrice;
+        li.appendChild(p);
+        li.appendChild(p2);
+        seatContainer.appendChild(li);
+       
 
         
 
